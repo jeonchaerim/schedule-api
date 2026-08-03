f73416278b9221cc9a69f2913a012b5c2d6dc7a9
jeonchaerim <105492421+jeonchaerim@users.noreply.github.com>
2026-08-03 18:49:09 +0900

    feat: Spring Boot 프로젝트 초기 설정

---
+++ src/test/java/io/github/jeonchaerim/schedule_api/ScheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jeonchaerim.schedule_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
